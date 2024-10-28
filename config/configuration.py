b641e55de5286edb45a947f3e6add373e224890e
BGbigbear <cll20619@qq.com>
2024-10-28 15:51:53 +0800

    add corpus

--- config/configuration.py
+++ config/configuration.py
@@ -5,8 +5,8 @@ db_path = "C:/Users/sibin/worksp/COMPETITION/Causality Extraction/database/faiss
 test_file = './data/initial/train1.json'
 causality_file = './data/reconstruction/train1_1.json'
 
-analysis_file = "./result/causality_train1_analysis_rougeSFT_full_1.json"
-pred_file = './result/causality_train1_predict_rougeSFT_full_1.json'
+analysis_file = "./result/causality_train1_analysis_rougeSFT_full_8.json"
+pred_file = './result/causality_train1_predict_rougeSFT_full_8.json'
 # analysis_file = "./result/causality_analysis_test.json"
 # pred_file = './result/causality_predict_test.json'
 
